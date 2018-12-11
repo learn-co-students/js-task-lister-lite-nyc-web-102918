d3b577089faf4a225e9feca6abd176a179a5d609
jbmenashi <jbmenashi@gmail.com>
2018-12-11 17:12:27 -0500

    add functions

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,36 @@
-
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+
+  const form = document.querySelector('#create-task-form');
+  const task = document.querySelector('#tasks');
+
+
+  form.addEventListener('submit',function (event) {
+    event.preventDefault()
+
+    const newTask = event.target.querySelector('#new-task-description').value
+    const newLiTag = document.createElement('li')
+    newLiTag.innerText = newTask
+
+    const priority = document.querySelector('#color-dropdown')
+    if (priority.value === 'high') {
+      newLiTag.style.color = "red"
+    }
+    else if (priority.value === 'medium') {
+      newLiTag.style.color = "yellow"
+    }
+    else {
+      newLiTag.style.color = "green"
+    }
+
+
+    const deleteButton = document.createElement('button')
+    deleteButton.addEventListener('click', function(event) {
+      newLiTag.remove()
+    })
+
+    deleteButton.innerText = 'X'
+    newLiTag.appendChild(deleteButton)
+    task.appendChild(newLiTag)
+  })
 });
