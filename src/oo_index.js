70a210704c9752fa913309eeac70dbe43dec3304
gavinmoore36 <gavizoid16@gmail.com>
2018-12-11 17:38:27 -0500

    done yall

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,6 +1,14 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const task = document.querySelector(‘#new-task-description’)
-  task.addEventListener('click', function (event) {
-
+  const task = document.querySelector('#create-task-form')
+  const listOfTasks = document.querySelector('#tasks')
+  task.addEventListener('submit', function (event) {
+    event.preventDefault()
+    // Getting the form and preventing it to post on a different page
+    const newTask = event.target.querySelector('#new-task-description').value
+    //Getting the value of the description inputted within the form
+    const newLiTag = document.createElement('li')
+    newLiTag.textContent = newTask
+    listOfTasks.appendChild(newLiTag)
+  })
 });
