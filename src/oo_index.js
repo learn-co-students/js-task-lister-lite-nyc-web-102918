1b4a2944580e41d9eb7316a46c3dcd48650391b7
GermainDuran <arturodurantorres@gmail.com>
2018-12-11 16:26:29 -0500

    Pair Programing Mod 3

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,20 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
-});
+
+  const form=document.querySelector('#create-task-form');
+  const task=document.querySelector('#tasks');
+
+  form.addEventListener('submit',function (event){
+
+  event.preventDefault()
+  const  newTask = event.target.querySelector('#new-task-description').value
+  //console.log(newTask)
+
+  const newLiTag = document.createElement('li')
+  newLiTag.innerText= newTask
+
+  task.appendChild(newLiTag)
+  }
+
+)});
