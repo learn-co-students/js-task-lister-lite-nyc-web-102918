c2a18994364f6de605b67bdb745eab1f2c0d508f
vibhu mahendru <vibhumahendru@vibhus-MacBook-Pro.local>
2018-12-11 17:34:53 -0500

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,6 @@
-
+document.addEventListener('DOMContentLoaded', function domContentHandler() {
+  console.log('WHAT IS THE FORM? ', document.querySelector('#comment-form'))
+  console.log('%c Hello lol', 'color:goldenrod')})
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
 });
--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,42 @@
+document.addEventListener('DOMContentLoaded', function domContentHandler() {
+  console.log('WHAT IS THE FORM? ', document.querySelector('#comment-form'))
+  console.log('%c Hello lol', 'color:goldenrod')
+
+  const form = document.querySelector('#create-task-form')
+
+    form.addEventListener('submit', function newTaskHandler(event){
+    event.preventDefault()
+
+
+    const newTask = document.querySelector('#new-task-description').value
+
+    const newLiTag = document.createElement('li')
+
+    newLiTag.innerText = newTask
+
+    const container = document.querySelector('#list')
+
+    const button = document.createElement('button')
+
+    button.innerText = 'X'
+
+    button.addEventListener('click', function(event){
+      newLiTag.remove()
+    } )
+
+    container.appendChild(newLiTag)
+    newLiTag.appendChild(button)
+    event.target.reset()
+
+
+
+  })
+
+
+
+})
+
 class TaskList {
+
+
 }
